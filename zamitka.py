f10339660c30508dd75b7b9665d73ad9f1b31850
DieFirrex <154069304+DieFirrex@users.noreply.github.com>
2023-12-28 17:05:02 +0200

    Add files via upload

---
+++ zamitka.py
@@ -0,0 +1,52 @@
+from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton,
+                             QLabel, QVBoxLayout, QHBoxLayout, QLineEdit, QListWidget)
+
+app = QApplication([])
+window = QWidget()
+
+
+
+window.resize(250,250)
+window.move(560,225)
+
+button1 = QPushButton('добавити')
+button2= QPushButton('видалити')
+button3 = QPushButton('зберегти')
+
+line = QLineEdit()
+list1 = QListWidget()
+
+v = QVBoxLayout()
+v.addWidget(line)
+v.addWidget(list1)
+v.addWidget(button1)
+v.addWidget(button2)
+v.addWidget(button3)
+window.setLayout(v)
+
+tasks = []
+def add_task():
+    t = line.text()
+    if t:
+        tasks.append(t)
+        list1.addItem(t)
+        line.clear()
+button1.clicked.connect(add_task)
+
+def remove_task():
+    t = list1.currentRow()
+    if t >= 0:
+        del tasks[t]
+        list1.takeItem(t)
+button2.clicked.connect(remove_task)
+
+def save_task():
+    with open("tasks.txt","w") as file:
+        for t in tasks:
+            file.write(t+'\n')
+button3.clicked.connect(save_task)
+
+
+
+window.show()
+app.exec_()
